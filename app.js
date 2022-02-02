6009da03c1b872d652d2cc86b931dddd12884169
Jkirvesmaki <63333403+Jkirvesmaki@users.noreply.github.com>
2022-02-02 14:22:33 +0200

    uibuild

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
+app.use(express.static('build'))
 app.use('/api/books', booksRouter)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
