d1ed838e38980747d02660056171835691559d55
Jkirvesmaki <63333403+Jkirvesmaki@users.noreply.github.com>
2022-02-02 11:53:20 +0200

    heroku

--- src/App.js
+++ src/App.js
@@ -1,25 +1,135 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Book from "./components/Book";
+import bookService from "./services/books";
 
-function App() {
+const App = () => {
+  const [books, setBooks] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [notification, setNotification] = useState(null);
+  const [newBookTitle, setNewBookTitle] = useState("");
+  const [newBookAuthor, setNewBookAuthor] = useState("");
+  const [newBookDescription, setNewBookDescription] = useState("");
+  const [bookId, setBookId] = useState("");
+
+  useEffect(() => {
+    bookService.getAll().then((initialBooks) => {
+      setBooks(initialBooks);
+    });
+  }, []);
+
+  const addBook = (event) => {
+    event.preventDefault();
+    const bookObject = {
+      author: newBookAuthor,
+      title: newBookTitle,
+      description: newBookDescription,
+    };
+    bookService.create(bookObject).then((returnedBook) => {
+      setBooks(books.concat(returnedBook));
+    });
+    setNotification("New book " + bookObject.title + " was added!");
+    setNewBookTitle("");
+    console.log(newBookTitle);
+    setNewBookAuthor("");
+    setNewBookDescription("");
+  };
+
+  const updateBook = (bookId, book) => {
+    const bookObject = {
+      author: newBookAuthor,
+      title: newBookTitle,
+      description: newBookDescription,
+    };
+
+    bookService.update(bookId, bookObject);
+    setNotification("New book " + bookObject.title + " was updated!");
+    setNewBookTitle("");
+
+    setNewBookAuthor("");
+    setNewBookDescription("");
+  };
+
+  const deleteBook = (id) => {
+    if (id != "") {
+      bookService.deleteById(id);
+
+      setNotification("book was deleted!");
+      setNewBookTitle("");
+      setNewBookAuthor("");
+      setNewBookDescription("");
+      setBookId("");
+    } else {
+      setNotification("No id was given");
+    }
+  };
+
+  const handleBookTitleChange = (event) => {
+    console.log(event.target.value);
+    setNewBookTitle(event.target.value);
+  };
+  const handleBookAuthorChange = (event) => {
+    console.log(event.target.value);
+    setNewBookAuthor(event.target.value);
+  };
+  const handleBookDescriptionChange = (event) => {
+    console.log(event.target.value);
+    setNewBookDescription(event.target.value);
+  };
+
+  const bookForm = () => (
+    <form onSubmit={addBook}>
+      <div>
+        <label>
+          Title:
+          <input value={newBookTitle} onChange={handleBookTitleChange} />
+        </label>
+      </div>
+      <div>
+        <label>
+          Author:
+          <input value={newBookAuthor} onChange={handleBookAuthorChange} />
+        </label>
+      </div>
+      <div>
+        <label>
+          Description:
+          <input
+            value={newBookDescription}
+            onChange={handleBookDescriptionChange}
+          />
+        </label>
+      </div>
+      <button type="submit">Save new</button>
+    </form>
+  );
+
+  const handleBookClick = (book) => {
+    setNewBookTitle(book.title);
+    setNewBookAuthor(book.author);
+    setNewBookDescription(book.description);
+    setBookId(book.id);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <h1>Books</h1>
+        {notification}
+        {bookForm()}
+        <button onClick={() => deleteBook(bookId)}>Delete</button>
+        <button onClick={() => updateBook(bookId)}>Save</button>
+      </div>
+      <div>
+        <ul>
+          {books.map((book) => (
+            <button className="book" onClick={() => handleBookClick(book)}>
+              <Book key={book.id} book={book} />
+            </button>
+          ))}
+        </ul>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
